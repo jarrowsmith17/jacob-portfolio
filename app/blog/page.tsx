9c7cdb1484cd19ddb4a222e628e33ebc7ce4a78b
Jacob <jacob.arrowsmith17@gmail.com>
2026-02-06 19:03:57 +0000

    dynamically render blogs

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from '@/app/lib/prisma';
 import BlogList from './BlogList'; 
 
+export const dynamic = 'force-dynamic';
+
 export default async function BlogPage() {
   const posts = await prisma.blogPost.findMany({
     where: { published: true },
